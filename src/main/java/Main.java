794f21c87387dfdc28f8d1927e5a349df223cd57
Lucas Ivan <lucas.ivan.medeiros@gmail.com>
2021-02-28 12:22:12 -0300

    Integração com Maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Main {
+public class Main {
 
     public static int converteInt(String integer) {
         try {
